ee2dad86965803c07a832a64cda496ccfdb698b8
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2023-08-31 10:23:34 -0300

    Create asdsd.java

---
+++ asdsd.java
@@ -0,0 +1,17 @@
+package com.company;
+public class asdsd {
+
+    public static void main(String[] args) {
+
+        double a = 91.37, b = 7.351;
+
+        double soma = a + b;
+
+        double prod = a * b;
+
+
+        System.out.println(soma);
+        System.out.println(prod);
+    }
+
+}
